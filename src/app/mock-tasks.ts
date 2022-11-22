96707a93735dbc316bb61e0cc56f3e30adf99139
Masihdev <masihullahmassudi85@gmail.com>
2022-11-22 11:04:18 +0100

    Angular fontawesome added

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+import { Task } from './Tasks';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Doctors Appointment',
+    day: 'May 5th 2:30pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Meeting at School',
+    day: 'May 6th 3:30pm',
+    reminder: true,
+  },
+  {
+    id: 1,
+    text: 'Food Shopping',
+    day: 'May 7th 4:30pm',
+    reminder: false,
+  },
+  {
+    id: 1,
+    text: 'Going to the Park',
+    day: 'May 8th 9:30pm',
+    reminder: false,
+  },
+];
