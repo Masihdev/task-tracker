b83cbe3a07e2106802cd510ab7df1110c5773cec
Masihdev <masihullahmassudi85@gmail.com>
2022-11-27 09:58:59 +0100

    new task can be added

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -11,6 +11,7 @@ export class TaskItemComponent implements OnInit {
   @Input()
   task!: Task;
   @Output() OnDeleteTask: EventEmitter<Task> = new EventEmitter();
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
   faTimes = faTimes;
 
   constructor() {}
@@ -19,5 +20,10 @@ export class TaskItemComponent implements OnInit {
 
   onDelete(task: any) {
     this.OnDeleteTask.emit(task);
+    this.onToggleReminder.emit(task);
+  }
+
+  onToggle(task: any) {
+    this.onToggleReminder.emit(task);
   }
 }
